d206db66faf1611ad85991c6db2051b188a6cabb
Antero Vainio <anterongugl@gmail.com>
2024-04-26 11:45:26 +0300

    Remove dataset constructor argument from SparsePyTorchSource and always use CIFAR10

--- applications.py
+++ applications.py
@@ -1,15 +1,30 @@
 import uuid
 
 from sparse_framework import SparseSource, SparseSink, SparseOperator
+
 import torch
 from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
 
 from .vgg import VGG_unsplit
 
 class SparsePyTorchSource(SparseSource):
-    def __init__(self, dataset, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.dataloader = DataLoader(dataset, 1)
+        self.dataloader = DataLoader(datasets.CIFAR10(
+            root = "/data",
+            train = True,
+            download = True,
+            transform = transforms.Compose([
+                transforms.Resize(size=(32, 32)),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
+                    )
+                ])
+            ),
+            1)
 
     def get_tuple(self):
         features, labels = next(iter(self.dataloader))
