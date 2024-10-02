f8d5db9cc858767e5d3c114029b379f25d62cce4
Antero Vainio <anterongugl@gmail.com>
2024-10-02 17:02:44 +0300

    Use Deployment dataclass

--- __main__.py
+++ __main__.py
@@ -1,18 +1,15 @@
 import os
 
-from sparse_framework import SparseAPIClient
+from sparse_framework import Deployment, SparseAPIClient
 
 if __name__ == "__main__":
     module_name = "VGGClassifier"
-    app = { "name": "VGGClassifier",
-            "dag": {
-                "Cifar10Source": {"VGGClassifier"},
-                "VGGClassifier": {"ArgMax"}
-                }
-            }
+    deployment = Deployment(name="VGGClassifier",
+                            dag={"Cifar10Source": {"VGGClassifier"},
+                                 "VGGClassifier": {"ArgMax"}})
     api_host = os.environ.get('SPARSE_API_HOST') or '127.0.0.1'
     api_port = os.environ.get('SPARSE_API_PORT') or 50006
     sparse_client = SparseAPIClient(api_host, api_port)
 
     sparse_client.create_module(module_name, module_dir='./sparse_module')
-    sparse_client.create_deployment(app)
+    sparse_client.create_deployment(deployment)
