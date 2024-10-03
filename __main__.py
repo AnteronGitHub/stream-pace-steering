9a2c8d51be2483dcdb5eb0eaa68b64ccb30345b4
Antero Vainio <anterongugl@gmail.com>
2024-10-03 12:52:24 +0300

    Parse deployment from yaml file

--- __main__.py
+++ __main__.py
@@ -3,13 +3,8 @@ import os
 from sparse_framework import Deployment, SparseAPIClient
 
 if __name__ == "__main__":
-    module_name = "VGGClassifier"
-    deployment = Deployment(name="VGGClassifier",
-                            dag={"Cifar10Source": {"VGGClassifier"},
-                                 "VGGClassifier": {"ArgMax"}})
-    api_host = os.environ.get('SPARSE_API_HOST') or '127.0.0.1'
-    api_port = os.environ.get('SPARSE_API_PORT') or 50006
-    sparse_client = SparseAPIClient(api_host, api_port)
+    sparse_client = SparseAPIClient(os.environ.get('SPARSE_API_HOST') or '127.0.0.1',
+                                    os.environ.get('SPARSE_API_PORT') or 50006)
 
-    sparse_client.create_module(module_name, module_dir='./sparse_module')
-    sparse_client.create_deployment(deployment)
+    sparse_client.create_module("VGGClassifier", module_dir='./sparse_module')
+    sparse_client.create_deployment(Deployment.from_yaml("vgg-deployment.yaml"))
