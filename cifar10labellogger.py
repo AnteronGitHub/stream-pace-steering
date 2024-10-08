ed5d025576826d244eb2961bf9499474c82860b1
Antero Vainio <anterongugl@gmail.com>
2024-10-08 12:15:30 +0300

    Log stream id on cifar label logger

--- cifar10labellogger.py
+++ cifar10labellogger.py
@@ -5,9 +5,9 @@ import os
 from sparse_framework import SparseSink
 
 class Cifar10LabelLogger(SparseSink):
-    def __init__(self):
+    def __init__(self, i : int):
         super().__init__()
-        self.logger = logging.getLogger("Cifar10LabelLogger")
+        self.logger = logging.getLogger(f"Cifar10LabelLogger-{i}")
         logging.basicConfig(format='[%(asctime)s] %(name)s - %(levelname)s: %(message)s', level=logging.INFO)
         self.classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
@@ -15,7 +15,7 @@ class Cifar10LabelLogger(SparseSink):
         self.logger.info("Class: %s", self.classes[new_tuple])
 
 async def start_sinks(host : str, no_sources = 10):
-    await asyncio.gather(*[Cifar10LabelLogger().connect(f"Cifar10Labels-{i}", host) for i in range(no_sources)])
+    await asyncio.gather(*[Cifar10LabelLogger(i).connect(f"Cifar10Labels-{i}", host) for i in range(no_sources)])
 
 if __name__ == "__main__":
     asyncio.run(start_sinks(os.environ.get("SPARSE_API_HOST") or "127.0.0.1"))
